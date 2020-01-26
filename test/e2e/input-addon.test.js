70e8137bc7d235d02d689aa71da683ebf5453821
Tom Coemans <tom.coemans@gmail.com>
2020-01-26 11:22:43 +0100

    before done vervangen door return

--- test/e2e/input-addon.test.js
+++ test/e2e/input-addon.test.js
@@ -5,16 +5,7 @@ const VlInputAddonPage = require('./pages/vl-input-addon.page');
 describe('vl-input-addon', async () => {
     const vlInputAddonPage = new VlInputAddonPage(driver);
 
-    before((done) => {
-        vlInputAddonPage.load().then(() => {
-            done()
-        });
+    before(() => {
+        return vlInputAddonPage.load();
     });
-
-  after((done) => {
-      if(driver) {
-          driver.quit();
-          done();
-      }
-  })
 });
