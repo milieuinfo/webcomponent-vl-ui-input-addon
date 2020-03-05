36ef68de6b768cdfcbb50f83eeec353dd9ff46ed
coemans <tom.coemans@gmail.com>
2020-03-05 13:15:12 +0100

    Update src/vl-input-addon.js
    Co-Authored-By: Sander Kleykens <sander.kleykens@avnu.be>

--- src/vl-input-addon.js
+++ src/vl-input-addon.js
@@ -38,7 +38,7 @@ export class VlInputAddon extends VlInputAddonElement(HTMLParagraphElement) {}
 /**
  * VlButtonInputAddon
  * @class
- * @classdesc Gebruik de button-input-addon in combinatie met de vl-ui-input-group webcomponent. Deze combinatie maakt de input-addon clickable.
+ * @classdesc Gebruik de vl-button-input-addon in combinatie met de vl-input-group webcomponent. Deze combinatie zorgt er voor dat men een button heeft die naast de input in vl-input-group staat.
  * 
  * @extends VlInputAddonElement
  * 
