9217c41a2237202baf48f3d788d7be98541def28
wautergu <guy.wauters@vlaanderen.be>
2020-03-05 10:07:06 +0100

    jsdoc toevoegen

--- src/vl-input-addon.js
+++ src/vl-input-addon.js
@@ -1,5 +1,18 @@
 import { NativeVlElement, define } from '/node_modules/vl-ui-core/dist/vl-core.js';
 
+
+/**
+ * VlInputAddonElemnt
+ * @class
+ * @classdesc Base klasse voor inputAddon elementen.
+ * 
+ * @extends NativeVlElement
+ * 
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-input-addon/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-input-addon/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-input-addon.html|Demo}
+ * 
+ */
 export const VlInputAddonElement = (SuperClass) => {
     return class extends NativeVlElement(SuperClass) {
         connectedCallback() {
@@ -22,6 +35,18 @@ export const VlInputAddonElement = (SuperClass) => {
  */
 export class VlInputAddon extends VlInputAddonElement(HTMLParagraphElement) {}
 
+/**
+ * VlButton-Input-addon
+ * @class
+ * @classdesc Gebruik de button-input-addon in combinatie met de vl-ui-input-group webcomponent. Deze combinatie maakt de input-addon clickable.
+ * 
+ * @extends VlInputAddonElement
+ * 
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-input-addon/releases/latest|Release notes}
+ * @see {@link https://www.github.com/milieuinfo/webcomponent-vl-ui-input-addon/issues|Issues}
+ * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-input-addon.html|Demo}
+ * 
+ */
 export class VlButtonInputAddon extends VlInputAddonElement(NativeVlElement(HTMLButtonElement)) {} 
 
 
