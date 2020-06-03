e48a27a50a63a8f5fd40c7d2e1425b013c91ef7d
Tom Coemans <tom.coemans@gmail.com>
2020-06-03 13:43:29 +0200

    ESlint

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-module.exports = { 
-    Test: {
-        VlInputAddon: require('./test/e2e/components/vl-input-addon').VlInputAddon,
-        VlButtonInputAddon: require('./test/e2e/components/vl-input-addon').VlButtonInputAddon
-    }
-}
+module.exports = {
+  Test: {
+    VlInputAddon: require('./test/e2e/components/vl-input-addon').VlInputAddon,
+    VlButtonInputAddon: require('./test/e2e/components/vl-input-addon').VlButtonInputAddon,
+  },
+};
--- test/e2e/components/vl-input-addon.js
+++ test/e2e/components/vl-input-addon.js
@@ -1,22 +1,21 @@
-const { VlElement } = require('vl-ui-core').Test;
-const { By } = require('selenium-webdriver');
+const {VlElement} = require('vl-ui-core').Test;
+const {By} = require('selenium-webdriver');
 
-class VlInputAddon extends VlElement {  
+class VlInputAddon extends VlElement {
 }
 
 class VlButtonInputAddon extends VlInputAddon {
-
-    async getIcon() {
-        const icon = await this.findElement(By.css(this.selector + ' [is="vl-icon"]'));
-        if (icon)  {
-            const { VlIcon } = require('vl-ui-icon').Test;
-            return new VlIcon(this.driver, icon);
-        } 
+  async getIcon() {
+    const icon = await this.findElement(By.css(this.selector + ' [is="vl-icon"]'));
+    if (icon) {
+      const {VlIcon} = require('vl-ui-icon').Test;
+      return new VlIcon(this.driver, icon);
     }
- }
+  }
+}
 
 module.exports = {
-    VlInputAddon,
-    VlButtonInputAddon
+  VlInputAddon,
+  VlButtonInputAddon,
 };
-    
+
