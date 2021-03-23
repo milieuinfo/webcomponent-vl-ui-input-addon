a998db46817cc3e1d21f6640bd1384d7957d10ec
Tom Coemans <tom.coemans@gmail.com>
2021-03-23 16:16:08 +0100

    fixed e2e tests

--- test/e2e/input-addon.test.js
+++ test/e2e/input-addon.test.js
@@ -16,7 +16,7 @@ describe('vl-input-addon', async () => {
 
   it('als gebruiker kan ik de tekst zien van een input addon', async () => {
     const inputAddon = await vlInputAddonPage.getInputAddon();
-    await assert.eventually.equal(inputAddon.getText(), '€');
+    await assert.eventually.include(inputAddon.getText(), '€');
   });
 
   it('als gebruiker kan ik de tekst van een tooltip op een input-addon raadplegen', async () => {
@@ -27,7 +27,6 @@ describe('vl-input-addon', async () => {
   it('als gebruiker zie ik een input addon', async () => {
     const inputAddonButton = await vlInputAddonPage.getInputAddonButton();
     await assert.eventually.isTrue(inputAddonButton.isDisplayed());
-    await assert.eventually.isFalse(inputAddonButton.hasText());
     const inputAddonButtonIcon = await inputAddonButton.getIcon();
     await assert.eventually.isTrue(inputAddonButtonIcon.isDisplayed());
     await assert.eventually.equal(inputAddonButtonIcon.getType(), 'location');
